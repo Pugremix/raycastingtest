2aa6f70d3fe6bdcd4e8bc988e0117fa106b86c1a
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-06 14:45:31 -0600

    Changes

--- output.py
+++ output.py
@@ -11,4 +11,6 @@ class Player:
         self.p_angle = p_angle
     def Draw_internal_player(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(self.pX, self.pY, 15, 15))
+        line_x = self.pX + math.cos(math.radians(0))
+        line_y = self.pY + math.sin(math.radians(0))
         pygame.draw.line(screen, (0, 0, 0), (self.pX + 7, self.pY + 7), (self.pX + 7, self.pY + 100), 3)
