f0a850ee6a2900434e52c14e6b03597edf664dc5
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-06 14:19:16 -0600

    Changes

--- output.py
+++ output.py
@@ -4,8 +4,9 @@ pygame.init()
 
 # Internal view
 class Player:
-    def __init__(self, pX, pY):
+    def __init__(self, pX, pY, p_angle):
         self.pX = pX
         self.pY = pY
+        self.p_angle = p_angle
     def Draw_internal_player(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(self.pX, self.pY, 20, 20))
