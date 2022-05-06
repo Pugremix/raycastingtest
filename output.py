4d5fb39fd953222478b89be23b80654a3b203ef0
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-06 14:48:08 -0600

    Changes

--- output.py
+++ output.py
@@ -5,12 +5,13 @@ pygame.init()
 
 # Internal view
 class Player:
-    def __init__(self, pX, pY, p_angle):
+    def __init__(self, pX, pY, p_angle, sight):
         self.pX = pX
         self.pY = pY
         self.p_angle = p_angle
+        self.sight = sight
     def Draw_internal_player(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(self.pX, self.pY, 15, 15))
-        line_x = self.pX + math.cos(math.radians(0))
-        line_y = self.pY + math.sin(math.radians(0))
-        pygame.draw.line(screen, (0, 0, 0), (self.pX + 7, self.pY + 7), (self.pX + 7, self.pY + 100), 3)
+        line_x = self.pX + 7 + math.cos(math.radians(0))
+        line_y = self.pY + 7 + math.sin(math.radians(0))
+        pygame.draw.line(screen, (0, 0, 0), (self.pX + 7, self.pY + 7), (line_x, line_y), 3)
