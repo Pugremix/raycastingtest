4065a035ff57ab220be0a0e1e2b8cb2dfaa59357
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-09 14:33:54 -0600

    test

--- processing.py
+++ processing.py
@@ -6,11 +6,8 @@ pygame.init()
 # Ray Distance
 class Ray_Calculation:
     def __init__(self, pX, pY, p_angle):
+        self.grid = 9
+        self.size = 45
         self.Ray_Target = 0
         self.target_x = pX - math.sin(p_angle)
         self.target_y = pY + math.cos(p_angle)
-# Map
-class Map:
-    def __init__(self):
-        self.grid = 9
-        self.size = 45
