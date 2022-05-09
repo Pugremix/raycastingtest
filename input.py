5b50c135c553416674bd3cee72bb10d7afaac4c5
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-09 11:43:56 -0600

    test

--- input.py
+++ input.py
@@ -7,8 +7,7 @@ class Movement:
     def __init__(self, angle=90):
         self.angle = angle
         self.rotation_speed = 5
-    def Movement(self):
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            self.angle -= self.rotation_speed
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            self.angle += self.rotation_speed
+    def Rotate_Left(self):
+        self.angle -= self.rotation_speed
+    def Rotate_Right(self):
+        self.angle += self.rotation_speed
