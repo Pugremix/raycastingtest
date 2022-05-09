1e626a317999f595bbc87794d4aa9e85b5700a9a
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-09 14:27:59 -0600

    test

--- processing.py
+++ processing.py
@@ -4,6 +4,16 @@ import pygame
 pygame.init()
 
 # Ray Distance
-Ray_Target = 0
-target_x = 0
-target_y = 0
+class Ray_Calculation():
+    Ray_Target = 0
+    target_x = pX - math.sin(p_angle)
+    target_y = pY + math.cos(p_angle)
+# Map
+class Map:
+    def __init__(self):
+        self.grid = 9
+        self.size = 45
+        self.color = (160, 160, 160)
+        self.empty = (100, 100, 100)
+    def Calulate(self):
+        tile = row * self.grid + col
