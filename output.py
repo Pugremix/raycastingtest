f23cf7dcc8059b3a3953fcfb82b77c8f90bb1198
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-16 13:33:56 -0600

    test

--- output.py
+++ output.py
@@ -29,5 +29,5 @@ def draw_vision(screen, darkness, pixel):
     if darkness > 180:
         darkness = 180
     pygame.draw.rect(
-        screen, (180 - darkness, 180 - darkness, 180 - darkness), pygame.Rect(400 + (pixel * 50), 500, 50, 100)
+        screen, (180 - darkness, 180 - darkness, 180 - darkness), pygame.Rect(420 + (pixel * 15), 500, 15, 165)
     )
--- processing.py
+++ processing.py
@@ -13,19 +13,19 @@ class Player:
     def internal_player(self, screen):
         output.draw_hitbox(screen, self.pX, self.pY)
     def internal_vision(self, screen, map, angle):
-        for rays in range(-6, 7):
+        for rays in range(-21, 22):
             # print('Start loop')
             distance = 0
             for sight in range(1, 45):
-                line_x = self.pX + 7 + math.cos(math.radians(angle + (rays * 3))) * (sight * 5)
-                line_y = self.pY + 7 + math.sin(math.radians(angle + (rays * 3))) * (sight * 5)
+                line_x = self.pX + 7 + math.cos(math.radians(angle + (rays * 1.25))) * (sight * 5)
+                line_y = self.pY + 7 + math.sin(math.radians(angle + (rays * 1.25))) * (sight * 5)
                 tile = identify_tile(line_x, line_y, map)
                 if tile == '_':
                     distance += 1
                 elif tile == '#':
                     break
                 output.draw_sight(screen, self.pX, self.pY, line_x, line_y)
-                output.draw_vision(screen, (distance * 3), rays)
+                output.draw_vision(screen, (distance * 4), rays)
                 # print(distance)
 # Identify tiles
 def identify_tile(line_x, line_y, map):
