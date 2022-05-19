c8a2e622b2a53c36f9fd21785d02c9f23faeca98
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-19 12:08:12 -0600

    test

--- main.py
+++ main.py
@@ -55,8 +55,3 @@ while running:
 
 # Quit
 pygame.quit()
-
-# Assistant line rotation code credit: https://stackoverflow.com/questions/18179877/pygame-rotating-a-line
-# Tile mapping code credit: https://www.youtube.com/watch?v=gxyKOGrqPq4&list=PLLfIBXQeu3abhbqWp4yUTYi7hWNOsEpXa&index=1
-# (Don't worry, the lack of libraries in this example inhibit
-# me from using most of the actual raycasting code as he has programmed)
