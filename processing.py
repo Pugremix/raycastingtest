81d54aeb135fb4eb1afcdd3792ed401a13ab2e33
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-26 11:29:16 -0600

    test

--- processing.py
+++ processing.py
@@ -14,18 +14,24 @@ class Player:
         output.draw_hitbox(screen, self.pX, self.pY)
     def internal_vision(self, screen, map, angle):
         for rays in range(-32, 33):
+            sight_range = 45
+            current_tile = None
             distance = 0
-            distance_mult = 0
-            for sight in range(1, 45):
+            distance_mod = 0
+            for sight in range(1, sight_range):
                 line_x = self.pX + 7 + math.cos(math.radians(angle + (rays * 1))) * (sight * 5)
                 line_y = self.pY + 7 + math.sin(math.radians(angle + (rays * 1))) * (sight * 5)
                 dist, tile = identify_tile(line_x, line_y, map)
                 if dist == '_':
                     distance += 1
+                    if not current_tile == tile:
+                        distance_mod += 1
                 elif dist == '#':
                     break
+                if distance == (sight_range - 1):
+                    distance_mod = 0
                 output.draw_sight(screen, self.pX, self.pY, line_x, line_y)
-                output.draw_vision(screen, (distance * 3.75), rays)
+                output.draw_vision(screen, ((distance * 4.5) - distance_mod), rays)
 
 # Identify tiles
 def identify_tile(line_x, line_y, map):
