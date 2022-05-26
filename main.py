f9f5ff85b0cac82fe1a6e0eb3d3cb45ab0174326
pugremix585@gmail.com <C0nn0rQu1nn>
2022-05-26 11:44:51 -0600

    test

--- main.py
+++ main.py
@@ -11,10 +11,20 @@ pygame.init()
 screen = pygame.display.set_mode([900, 750])
 pygame.display.set_caption("Raycasting Test")
 
+# Errors
+HasError = False
+ErrorFont = pygame.font.Font(pygame.font.get_default_font(), 35)
+
 # Objects
 # Hitbox
-Hitbox_Movement = input.Movement()
-Hitbox = processing.Player(Hitbox_Movement.pX, Hitbox_Movement.pY)
+try:
+    Hitbox_Movement = input.Movement()
+    Hitbox = processing.Player(Hitbox_Movement.pX, Hitbox_Movement.pY)
+except:
+    screen.fill((0, 0, 0))
+    text_surface = ErrorFont.render('Module Not Found', False, (255, 255, 255))
+    screen.blit(text_surface, (10, 10))
+    HasError = True
 
 # Run until user asks to quit loop
 running = True
@@ -26,32 +36,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
+    if (HasError != True):
 
-    if keys[pygame.K_LEFT]:
-        Hitbox_Movement.Rotate_Left()
-    if keys[pygame.K_RIGHT]:
-        Hitbox_Movement.Rotate_Right()
-    if keys[pygame.K_UP]:
-        Hitbox_Movement.Move_Forwards()
-    if keys[pygame.K_DOWN]:
-        Hitbox_Movement.Move_Backwards()
+        keys = pygame.key.get_pressed()
 
-    # Draw
-    # Background
-    screen.fill((20, 20, 20))
+        if keys[pygame.K_LEFT]:
+            Hitbox_Movement.Rotate_Left()
+        if keys[pygame.K_RIGHT]:
+            Hitbox_Movement.Rotate_Right()
+        if keys[pygame.K_UP]:
+            Hitbox_Movement.Move_Forwards()
+        if keys[pygame.K_DOWN]:
+            Hitbox_Movement.Move_Backwards()
 
-    # Map
-    Active_MAP = input.MAP_Templates
-    Board = output.Map()
-    Board.Draw_Map(screen, Active_MAP)
+        # Draw
+        # Background
+        screen.fill((20, 20, 20))
 
-    # View
+        # Map
+        Active_MAP = input.MAP_Templates
+        Board = output.Map()
+        Board.Draw_Map(screen, Active_MAP)
 
-    # Player
-    Hitbox = processing.Player(Hitbox_Movement.pX, Hitbox_Movement.pY)
-    Hitbox.internal_player(screen)
-    Hitbox.internal_vision(screen, Active_MAP, Hitbox_Movement.angle)
+        # View
+
+        # Player
+        Hitbox = processing.Player(Hitbox_Movement.pX, Hitbox_Movement.pY)
+        Hitbox.internal_player(screen)
+        Hitbox.internal_vision(screen, Active_MAP, Hitbox_Movement.angle)
 
     pygame.display.flip()
 
